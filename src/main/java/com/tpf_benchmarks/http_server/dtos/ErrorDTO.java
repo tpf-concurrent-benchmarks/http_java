a9d1ab6137458822a49a75104d985e45e6b96533
Facundo Mastricchio <facundomastricchio@gmail.com>
2024-05-06 23:10:14 -0300

    Add more exception handling

---
+++ src/main/java/com/tpf_benchmarks/http_server/dtos/ErrorDTO.java
@@ -0,0 +1,16 @@
+package com.tpf_benchmarks.http_server.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDTO {
+    @JsonProperty("error")
+    public String error;
+}
--- src/main/java/com/tpf_benchmarks/http_server/exceptions/GlobalControllerExceptionHandler.java
+++ src/main/java/com/tpf_benchmarks/http_server/exceptions/GlobalControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.tpf_benchmarks.http_server.exceptions;
 
+import com.tpf_benchmarks.http_server.dtos.ErrorDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +13,26 @@ public class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<String> handleUserNotFound(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorDTO> handleUserNotFound(UserNotFoundException ex) {
+        return new ResponseEntity<>(new ErrorDTO(ex.getMessage()), HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(UserIsNotCreatorException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<ErrorDTO> handleUserIsNotCreator(UserIsNotCreatorException ex) {
+        return new ResponseEntity<>(new ErrorDTO(ex.getMessage()), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(PollNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorDTO> handlePollNotFound(PollNotFoundException ex) {
+        return new ResponseEntity<>(new ErrorDTO(ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PollOptionNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorDTO> handlePollOptionNotFound(PollOptionNotFoundException ex) {
+        return new ResponseEntity<>(new ErrorDTO(ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
 }
