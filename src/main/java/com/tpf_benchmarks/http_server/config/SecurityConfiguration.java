581f54716d6c899846d112b3e3091037bbb89eef
Facundo Mastricchio <facundomastricchio@gmail.com>
2024-05-10 19:43:37 -0300

    White flag 2 endpoints by HTTP method

--- src/main/java/com/tpf_benchmarks/http_server/config/SecurityConfiguration.java
+++ src/main/java/com/tpf_benchmarks/http_server/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.tpf_benchmarks.http_server.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -45,6 +46,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers(whiteListedRoutes).permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/polls/**").permitAll()
                                 .anyRequest().authenticated()
                 )
 
